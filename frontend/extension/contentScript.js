20f452a3e3c988149dba2ad519a3154366aff174
Alexander Hultnér <ahultner@gmail.com>
2020-05-22 22:21:57 +0200

    Fetches first image from page, prints some data based on that

--- frontend/extension/contentScript.js
+++ frontend/extension/contentScript.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+console.log("Get Caged: Loaded!")
+
+let imgs = document.querySelectorAll("img")
+
+console.log(imgs, document)
+
+
+if( imgs?.length > 0 ){
+    let firstImg = imgs[0]
+    
+    console.log(
+        "First Image:", 
+        {src: firstImg.src, height: firstImg.height, width: firstImg.width},
+    )
+
+} else {
+    console.log("No images found on page")
+}
