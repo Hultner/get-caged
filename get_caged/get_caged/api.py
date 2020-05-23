3ced7b00e8022f62b40db2ff65ecccc96607107b
Alexander Hultnér <ahultner@gmail.com>
2020-05-23 16:31:38 +0200

    Connected last dots

--- get_caged/get_caged/api.py
+++ get_caged/get_caged/api.py
@@ -6,6 +6,7 @@ from PIL import Image
 from get_caged.cage_image import CageImage
 from get_caged.resize import resize_keep_aspect_ratio, create_resized_cage_image
 from get_caged.crop import crop_by_face_coordinates
+from get_caged.db import find_caged_image
 from get_caged.target_image import TargetImageSpec
 
 app = FastAPI()
@@ -14,16 +15,19 @@ app = FastAPI()
 @app.get("/cage")
 async def get_cage_image(width: int = 0, height: int = 0):
     target = TargetImageSpec(width=width, height=height)
-    cage_image = get_cage_image_from_spec(target)
+    # cage_image = get_cage_image_from_spec(target)
+    cage_image = find_caged_image(target)
 
-    resized_image, resized_by = resize_keep_aspect_ratio(image=cage_image.image_data,
-                                                         target_width=width,
-                                                         target_height=height)
+    resized_image, resized_by = resize_keep_aspect_ratio(
+        image=cage_image.image_data, target_width=width, target_height=height
+    )
     resized_cage_image = create_resized_cage_image(cage_image, resized_image)
-    new_cage_image = crop_by_face_coordinates(cage_image=resized_cage_image,
-                                              target_width=width,
-                                              target_height=height,
-                                              resized_by=resized_by)
+    new_cage_image = crop_by_face_coordinates(
+        cage_image=resized_cage_image,
+        target_width=width,
+        target_height=height,
+        resized_by=resized_by,
+    )
 
     buf = BytesIO()
     new_cage_image.image_data.save(buf, format="PNG")
@@ -33,11 +37,13 @@ async def get_cage_image(width: int = 0, height: int = 0):
 
 def get_cage_image_from_spec(target: TargetImageSpec):
     image = Image.open("jpg_cat.png")
-    cage_image = CageImage(id=1,
-                           width=400,
-                           height=462,
-                           aspect_ratio=(400/462),
-                           image_data=image,
-                           face_height_coord=200,
-                           face_width_coord=200)
+    cage_image = CageImage(
+        id=1,
+        width=400,
+        height=462,
+        aspect_ratio=(400 / 462),
+        image_data=image,
+        face_height_coord=200,
+        face_width_coord=200,
+    )
     return cage_image
