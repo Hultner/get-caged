51c60959d3d75f57c74813dedc46ed8358599add
Helena Strandberg <hejhej.helena@gmail.com>
2020-05-23 15:29:57 +0200

    Crop image based on head center position

---
+++ get_caged/get_caged/crop.py
@@ -0,0 +1,54 @@
+from PIL import ImageOps
+
+from get_caged.cage_image import CageImage
+
+from get_caged.resize import create_resized_cage_image
+
+
+def crop_by_face_coordinates(cage_image: CageImage,
+                             target_width: int,
+                             target_height: int,
+                             resized_by: str) -> CageImage:
+    """
+    Crops image to get face as much in the center as possible, to avoid
+    just getting a forehead or a chin for example.
+    :param cage_image: a CageImage with an image that needs to be cropped
+    :param target_width: the desired width
+    :param target_height: the desired height
+    :param resized_by: string saying if image was resized by height or width
+    :return: CageImage object with new height and cropped to keep face
+    """
+
+    if resized_by == "height":
+        # height is already target, crop width
+        top = 0
+        bottom = 0
+
+        image = cage_image.image_data
+        while image.width > target_width:
+            if cage_image.face_height_coord < (image.height - cage_image.face_height_coord):
+                left = 0
+                right = 1
+            else:
+                left = 1
+                right = 0
+            border = (left, top, right, bottom)
+            image = ImageOps.crop(image, border)
+    else:
+        # crop height
+        left = 0
+        right = 0
+
+        image = cage_image.image_data
+        while image.height > target_height:
+            if cage_image.face_height_coord < (image.height - cage_image.face_height_coord):
+                top = 0
+                bottom = 1
+            else:
+                top = 1
+                bottom = 0
+            border = (left, top, right, bottom)
+            image = ImageOps.crop(image, border)
+
+    new_cage_image = create_resized_cage_image(cage_image, image)
+    return new_cage_image
--- get_caged/tests/test_get_caged.py
+++ get_caged/tests/test_get_caged.py
@@ -1,6 +1,8 @@
 from PIL import Image
 
-from get_caged.resize import resize_keep_aspect_ratio
+from get_caged.cage_image import CageImage
+from get_caged.resize import resize_keep_aspect_ratio, create_resized_cage_image
+from get_caged.crop import crop_by_face_coordinates
 from get_caged import __version__
 
 
@@ -21,3 +23,26 @@ def test_resize_keep_aspect_ratio():
     assert round(image_aspect_ratio, 1) == round(new_aspect_ratio, 1)
     assert resized_image.height == new_height
     assert reized_by == "height"
+
+
+def test_crop_by_face_coordinates():
+    image = Image.open("jpg_cat.png")
+    cage_image = CageImage(id=1,
+                           width=400,
+                           height=462,
+                           aspect_ratio=(400/462),
+                           image_data=image,
+                           face_height_coord=200,
+                           face_width_coord=200)
+
+    new_width = 20
+    new_height = 20
+    resized_image, resized_by = resize_keep_aspect_ratio(image, new_width, new_height)
+    resized_cage_image = create_resized_cage_image(cage_image, resized_image)
+    new_cage_image = crop_by_face_coordinates(resized_cage_image, new_width, new_height, resized_by)
+    #new_cage_image.image_data.save('/Users/nena/Desktop/get-caged/get_caged/tests/test_cat.png', 'PNG')
+    assert new_cage_image.width == new_width
+    assert new_cage_image.height == new_height
+
+
+
