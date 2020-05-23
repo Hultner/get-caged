2e0f6a0ee0a6ff117e0d60c9f68fa5ba7247f44d
Alexander Hultnér <ahultner@gmail.com>
2020-05-23 14:34:08 +0200

    Load images can now load all the data, create CageImage object and move file when done, saving to database left

--- get_caged/get_caged/etl/load_images.py
+++ get_caged/get_caged/etl/load_images.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from typing import Tuple
+from contextlib import contextmanager
 
 from pprint import pprint
 
+from PIL import Image
 from pydantic import BaseSettings
 
-# from get_caged.cage_image import CageImage
+from get_caged.cage_image import CageImage
+# from get_caged.db import initialize_db, insert_image
 
 repository_root = Path("../../../")
 
@@ -16,30 +19,64 @@ class ETLSettings(BaseSettings):
     processed_images_folder: Path = repository_root / Path("images/processed/")
 
 
+@contextmanager
+def process_file(file_: Path):
+    """
+    Opens a context for processing a file,
+    moves it to processed files when done if no exception occurs.
+    """
+    try:
+        yield file_
+    finally:
+        destination = ETLSettings().processed_images_folder / file_.name
+        with destination.open(mode="xb") as fid:
+            fid.write(file_.read_bytes())
+        file_.unlink()
+
+
 def get_face_coords(name: str) -> Tuple[int, int]:
-    return [
-        int(coord)
-        for coord in name.split("__")[0].split("_")
-    ]
+    return [int(coord) for coord in name.split("__")[0].split("_")]
 
 
-def create_cage_img(img: Path) -> "CageImage":
-    face_x, face_y = get_face_coords(img.name)
-    return (face_x, face_y, img)
+def create_cage_img(img_path: Path) -> CageImage:
+    face_x, face_y = get_face_coords(img_path.name)
+    image = Image.open(img_path)
+    cage_image = CageImage(
+        id=None,
+        width=image.width,
+        height=image.height,
+        aspect_ratio=(image.width / image.height),
+        image_data=image,
+        face_width_coord=face_x,
+        face_height_coord=face_y,
+    )
+    # return (face_x, face_y, img_path, image, cage_image)
+    return cage_image
+
+
+def save_img(img: CageImage) -> None:
+    "Saves the cage image to the database"
+    pass
 
 
 def run_processing():
-    pprint(ETLSettings)
-    # pprint([f for f in ETLSettings().unprocessed_images_folder.iterdir()])
-    files = [
-        create_cage_img(f)
+    img_files = list(
+        f
         for f in ETLSettings().unprocessed_images_folder.iterdir()
         if f.is_file() and f.name != ".gitkeep"
-    ]
-    pprint(files)
+    )
 
-    # for f in ETLSettings().unprocessed_images_folder.iterdir()
-    return ETLSettings().unprocessed_images_folder
+    for file_ in img_files:
+        try:
+            with process_file(file_):
+                cage_img = create_cage_img(file_)
+                save_img(cage_img)
+        except Exception as err:
+            print(f"Error when saving file: {file_}")
+            print(err)
+            print("Continuing processing of files")
+
+    return img_files
 
 
 if __name__ == "__main__":
