26bf1b3ea8b7ab6f2bd54a81f7ad3b96f68b6009
Helena Strandberg <hejhej.helena@gmail.com>
2020-06-05 20:22:01 +0200

    Don't feth too small image. Use minimum amount random images.

--- frontend/extension/contentScript.js
+++ frontend/extension/contentScript.js
@@ -4,11 +4,16 @@ console.log("Get Caged: Loaded!")
 const backendUrl = "http://localhost:8000"
 let imgs = document.querySelectorAll("img")
 
-function getRandomElements(list, n = Math.floor(Math.random() * list.length) ){
+function getRandomElements(list, n = constrainedRandom(list.length) ){
     tmpList = [...list]
     return Array(n).fill().map( 
         _ => tmpList.splice(Math.floor(Math.random() * tmpList.length), 1)[0]
     )
+}
+
+function constrainedRandom(maxNum, minNum = 3) {
+	const minSafeNum = Math.min(Math.max(minNum, maxNum * 0.03), maxNum)
+	return Math.floor(Math.random()*(maxNum-minSafeNum+1)+minSafeNum);
 }
 
 console.log(imgs, document)
@@ -29,7 +34,11 @@ if( imgs?.length > 0 ){
         // console.log(img)
         // img.src = "https://images.askmen.com/1080x540/2016/11/11-084440-nicolas_cage_life_lessons.jpg"
         const {width, height} = img
-        img.src = `${backendUrl}/cage?width=${width}&height=${height}`
+	if ((width >= 12) && (height >= 12)) {
+        	img.src = `${backendUrl}/cage?width=${width}&height=${height}`
+	} else {
+		console.log("Image is too small, won't fetch cage image")
+	}
     }
 
 } else {
